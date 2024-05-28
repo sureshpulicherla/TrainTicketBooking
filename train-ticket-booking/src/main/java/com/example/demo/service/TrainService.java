30866dd3216c55a4d688ef1cb6d79e4a40c4d1a1
suresh pulicherla <140886721+sureshpulicherla@users.noreply.github.com>
2024-05-28 18:52:34 +0530

    Add files via upload

--- train-ticket-booking/src/main/java/com/example/demo/service/TrainService.java
+++ train-ticket-booking/src/main/java/com/example/demo/service/TrainService.java
@@ -12,33 +12,36 @@ public class TrainService {
 
 	private List<Ticket> tickets = new ArrayList<>();
 	private Map<String, List<Ticket>> seats = new HashMap<>();
-	
+
 	public TrainService() {
 		seats.put("A", new ArrayList<>());
 		seats.put("B", new ArrayList<>());
 	}
 
 	public Ticket puchaseTicket(String from, String to, User user, String section) {
-		Ticket ticket = new Ticket(from,to,user,20.0,section);
-		tickets.add(ticket);
-		
-		if(!seats.containsKey(section)) {
-			seats.put(section, new ArrayList<>());
+
+		String normalSection = section.toUpperCase();
+
+		if (!normalSection.equals("A") && !normalSection.equals("B")) {
+			throw new IllegalArgumentException("Invalid section. Section must be 'A' or 'B'.");
 		}
-		
-		seats.get(section).add(ticket);
+
+		Ticket ticket = new Ticket(from, to, user, 20.0, section);
+		tickets.add(ticket);
+
+		seats.putIfAbsent(normalSection, new ArrayList<>());
+		seats.get(normalSection).add(ticket);
 		return ticket;
 	}
 
-	
 	public Ticket getTicketDetails(User user) {
-		
-		for(Ticket ticket : tickets) {
-			if(ticket.getUser().getEmail().equals(user.getEmail())) {
+
+		for (Ticket ticket : tickets) {
+			if (ticket.getUser().getEmail().equals(user.getEmail())) {
 				return ticket;
 			}
 		}
-		
+
 		return null;
 	}
 
@@ -47,24 +50,25 @@ public class TrainService {
 	}
 
 	public boolean removeUser(User user) {
-		
+
 		Ticket ticket = getTicketDetails(user);
-		if(ticket!= null) {
+		if (ticket != null) {
 			tickets.remove(ticket);
 			seats.get(ticket.getSeat()).remove(ticket);
 			return true;
 		}
-		
+
 		return false;
 	}
+
 	public boolean updateUserSeat(User user, String newSection) {
-        Ticket ticket = getTicketDetails(user);
-        if (ticket != null) {
-            seats.get(ticket.getSeat()).remove(ticket);
-            ticket.setSeat(newSection);
-            seats.get(newSection).add(ticket);
-            return true;
-        }
-        return false;
-    }
+		Ticket ticket = getTicketDetails(user);
+		if (ticket != null) {
+			seats.get(ticket.getSeat()).remove(ticket);
+			ticket.setSeat(newSection);
+			seats.get(newSection).add(ticket);
+			return true;
+		}
+		return false;
+	}
 }
